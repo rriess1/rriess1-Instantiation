b5886f56028b5769a615689581debd14513f160a
rriess1 <robertwriess@yahoo.com>
2023-02-17 21:27:04 +0000

    first

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object cat = new Object();
+        return cat;
     }
 }
